6ac7453a446c83ec591e2b6ca8415c2315795228
Jenne van der Meer <jennevdmeer@impulze.net>
2021-09-26 11:12:19 +0200

    track recent votes/rating

--- Statistics/Statistics.php
+++ Statistics/Statistics.php
@@ -6,6 +6,10 @@ class Statistics
 {
     public float $rating;
     public int $votes;
+
+    public float $recentRating;
+    public int $recentVotes;
+    
     public float $recommended;
 
     public int $locationId;
@@ -15,6 +19,10 @@ class Statistics
     {
         $this->rating = (float) $response['averageRating'];
         $this->votes = (int) $response['numberReviews'];
+
+        $this->recentRating = (float) $response['last12MonthAverageRating'];
+        $this->recentVotes = (int) $response['last12MonthNumberReviews'];
+
         $this->recommended = (float) $response['percentageRecommendation'] / 100;
 
         $this->locationId = (int) $response['locationId'];
